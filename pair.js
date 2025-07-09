7bbb53773b17a09436ba94f502819f0d3e3018dc
Shanuwa-shani <shanushani20061126@gmail.com>
2025-07-09 19:17:37 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,20 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
+*_Pair Code Connected by shanu TECH*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『 WOW YOU'VE CHOSEN shani MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+*👻මෙය ඉතමත් පාහසුවෙන් ක්‍රියත්මක කලහැකි බොට් කෙනෙකි🤍*
+║❒ *Owner:* _https://wa.me/94719827972_
+
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029Vb6pI1VBqbrIwyeZr12t
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vb6pI1VBqbrIwyeZr12t
 ╚════════════════════════╝
 _____________________________________
 
