7ee66638342d19e9a19338354fb194659683ee4f
Shanuwa-shani <shanushani20061126@gmail.com>
2025-07-13 16:06:15 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,22 +57,19 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
+*_Pair Code Connected by shanu Rajapaksha*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『 WOW YOU'VE CHOSEN Quveen shani MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
-╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
-╚════════════════════════╝
+───⭓
+❒  • Chat with owner •
+❒ *GitHub:* __pendin_
+❒ *Author:* _wa.me/94719827972_
+❒ *YT:* _ඕනම වෙලාවෙදි අත්හරින මිනිස්සු "මට Miss වෙන එකමයී හොද!🙂
+└────────────⭓
 _____________________________________
 
 _Don't Forget To Give Star To My Repo_`
